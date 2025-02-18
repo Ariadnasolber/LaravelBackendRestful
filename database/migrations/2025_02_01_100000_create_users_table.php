29baa97e720d6ba74260bc6f945442a8adb940b0
Ariadnasolber <ariadnasolber@campus.monlau.com>
2025-02-18 16:31:25 +0100

    seeders hechos

--- database/migrations/2025_02_01_100000_create_users_table.php
+++ database/migrations/2025_02_01_100000_create_users_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable(); // Añadir esta línea para la verificación de correo
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            RolesTableSeeder::class,
+            UsersTableSeeder::class,
+            CoursesTableSeeder::class,
+            SubjectsTableSeeder::class,
+            AssignmentsTableSeeder::class,
+            CalendarEventsTableSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
